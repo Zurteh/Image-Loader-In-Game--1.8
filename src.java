8b3fb11e6fd663d2e1a6c54a5187b1654966d4bb
Zurteh <43295443+Zurteh@users.noreply.github.com>
2019-03-31 18:14:50 +0100

    Create src.java

---
+++ src.java
@@ -0,0 +1,3 @@
+this.mc.getTextureManager().bindTexture(new ResourceLocation("example.png/jpg"));
+     Gui.drawScaledCustomSizeModalRect(2, +2, 0, 0, 50 / 1, 50 / 1, 50 / 1, 50 /
+         1, 50 / 1, 50 / 1);
